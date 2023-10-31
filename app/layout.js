6f2b89b8a706f42e371f71a88196a3200c7169d0
abhishekchaturvedi-07 <abhishekchaturvedi789@gmail.com>
2023-11-01 01:16:16 +0530

    PRODUCT - 3)- CONTEXT Creation including CRUD operations

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import "./globals.css";
 import "bootstrap-material-design/dist/css/bootstrap-material-design.min.css";
 
 import { CategoryProvider } from "@/context/category";
+import { ProductProvider } from "@/context/product";
 import { SessionProvider } from "next-auth/react";
 import { TagProvider } from "@/context/tag";
 import { Toaster } from "react-hot-toast";
@@ -15,11 +16,13 @@ export default function RootLayout({ children }) {
       <SessionProvider>
         <CategoryProvider>
           <TagProvider>
-            <body>
-              <TopNav />
-              <Toaster />
-              {children}
-            </body>
+            <ProductProvider>
+              <body>
+                <TopNav />
+                <Toaster />
+                {children}
+              </body>
+            </ProductProvider>
           </TagProvider>
         </CategoryProvider>
       </SessionProvider>
